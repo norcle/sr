00fb38824fa0bbbc78a70ad3273bf70b702b8ebf
elnikovs <elnikov.s@gmail.com>
2021-11-10 18:27:11 +0300

    spec first

---
+++ spec/lib/parser/fonbet/sport_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe Parser::Fonbet::Sport do
+  let(:live_json) do
+    @multilang = Struct.new(:ru, :en)
+    @ru = JSON.parse(open('spec/fixtures/json/fonbet.json').read)
+    @multilang.new(@ru, @ru)
+  end
+  let(:event) do
+    { 'ru': live_json['ru']['events'].first, 'en': live_json['en']['events'].first }.with_indifferent_access
+  end
+  let(:sport) { described_class.new(sport_id: event['ru']['sportId'], live_json: live_json) }
+
+  before do
+    Parser::Fonbet::Event::Entity.clear_cache
+    Parser::Fonbet::League.clear_cache
+    Parser::Fonbet::Team.clear_cache
+  end
+
+  describe 'public' do
+    it 'parse' do
+      sport.parse
+      sport_db = Sport.find_by(name_ru: 'Баскетбол')
+      expect(sport_db).to be_truthy
+      expect(sport_db.name_en).to match('Баскетбол')
+    end
+  end
+end
