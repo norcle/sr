155cf359a5f8b573364af0730f150ff23763bb3f
elnikovs <elnikov.s@gmail.com>
2021-11-19 11:57:09 +0300

    try to run react

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,12 +2,24 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+//
+// import Rails from "@rails/ujs"
+// import Turbolinks from "turbolinks"
+// import * as ActiveStorage from "@rails/activestorage"
+// import "channels"
+//
+//
+//
+// Rails.start()
+// Turbolinks.start()
+// ActiveStorage.start()
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+console.log('Hello world');
+ReactDOM.render(
+    <h1>Привет, мир!</h1>,
+    document.getElementById('root')
+);
+
