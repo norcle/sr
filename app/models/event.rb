5b5741c4d32f11d50c26952c43a71680d7f0a373
elnikov <elnikov.s@gmail.com>
2022-07-24 09:46:07 +0300

    add state machine to event

--- app/models/event.rb
+++ app/models/event.rb
@@ -33,7 +33,37 @@ class Event < ApplicationRecord
 
   after_touch { p "Ooohh... You touch my talalal #{id}" }
 
+  enum state: {
+    not_started: 1,
+    live: 2,
+    ended: 3,
+    cancelled: 4,
+  }
+
+
   def parent
     Event.find(parent_id)
   end
+
+  # state_machine :state, initial: :created do
+  #   event :confirm_payment do
+  #     transition created: :processing
+  #   end
+  #   event :pack do
+  #     transition processing: :ready
+  #   end
+  #   event :cancel do
+  #     transition %i[created processing ready] => :void
+  #   end
+  #   event :return do
+  #     transition delivered: :void
+  #   end
+  #   event :ship do
+  #     transition ready: :shipped
+  #   end
+  #   event :fail_delivery do
+  #     transition shipped: :processing
+  #   end
+  # end
+
 end
